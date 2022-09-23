806fe76d4e00a0686cea6c40602b3138e731e4a8
Kaja Jaronska <kaja.jaronska@gmail.com>
2022-09-23 14:45:44 +0100

    Update Player factory function to make variables unaccesible from the global scope

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 // Factory function creating players
-const player = (name,symbol) => {
+const Player = (name,symbol) => {
+    let score = 0;
+    const getName = () => name;
+    const getSymbol = () => symbol;
+    const getScore = () => score;
     return {
-        name,symbol,    //shorthand
-        score: 0
+        getName,getScore,getSymbol   //shorthand
     }
 }
 
-const player1 = player('Kaja','X');
-const player2 = player('Michael','O');
+const player1 = Player('Kaja','X');
+const player2 = Player('Michael','O');
 
-console.log(player1,player2)
+// console.log(player1.getScore())
+
 
 // Game flow module returning object - activePlayer
 const gameFlow = (()=> {
@@ -25,16 +29,32 @@ const gameFlow = (()=> {
     }
 })();
 
+// activePlayer = 'Kaja';
+
+// console.log(activePlayer);
+
 // Gameboard - module, because we only need one
-const gameBoard = (() => {
-    return {
-    // [0, 1, 2]
-    a: ['','',''],
-    b: ['','',''],
-    c: ['','','']}
+const game = (() => {
+
+    let board = {
+        // [0, 1, 2]
+    a: ['X','O','X'],
+    b: ['O','X','O'],
+    c: ['O','O','X']}
+    
+    // Function that will render the contents of the gameboard array to the webpage
+    let displayBoard = () => {
+        
+    }
+
+
+    return {board}
+
 })()
 
-console.log(gameBoard)
+
+// console.log(gameBoard.a)
+
 
 // Display Controller - module, because we only need one
 const displayController = (() => {
@@ -48,3 +68,35 @@ const displayController = (() => {
     })
 
 })()
+
+
+
+// const Player = (name, level) => {
+//     let health = level * 2;
+//     const getLevel = () => level;
+//     const getName  = () => name;
+//     const die = () => {
+//       // uh oh
+//     };
+//     const damage = x => {
+//       health -= x;
+//       if (health <= 0) {
+//         die();
+//       }
+//     };
+//     const attack = enemy => {
+//       if (level < enemy.getLevel()) {
+//         damage(1);
+//         console.log(`${enemy.getName()} has damaged ${name}`);
+//       }
+//       if (level >= enemy.getLevel()) {
+//         enemy.damage(1);
+//         console.log(`${name} has damaged ${enemy.getName()}`);
+//       }
+//     };
+//     return {attack, damage, getLevel, getName};
+//   };
+
+//   const player1 = Player('Kaja','3');
+
+//   console.log(player1.getLevel())
