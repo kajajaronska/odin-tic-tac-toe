f4e57bac72b6d8110fc97695d5fde13a72b5254b
Kaja Jaronska <kaja.jaronska@gmail.com>
2022-09-23 16:32:00 +0100

    Add functionality enabling active user to place his mark in the chosen spot on the game board. This updates the display as well as the game array.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Factory function creating players
 const Player = (name,symbol) => {
     let score = 0;
+    // Add variables to represent activePlayer, roundwinnner, gamewinner true/false 
     const getName = () => name;
     const getSymbol = () => symbol;
     const getScore = () => score;
@@ -9,15 +10,15 @@ const Player = (name,symbol) => {
     }
 }
 
+
 const player1 = Player('Kaja','X');
 const player2 = Player('Michael','O');
-
-// console.log(player1.getScore())
 
 
 // Game flow module returning object - activePlayer
 const gameFlow = (()=> {
-    let activePlayer;
+    let activePlayer = player1;
+
     let roundWinner; // after every round 
     let gameWinner;
     let roundCounter; // decrease by one after every round, once ZERO - announce game winner
@@ -29,9 +30,6 @@ const gameFlow = (()=> {
     }
 })();
 
-// activePlayer = 'Kaja';
-
-// console.log(activePlayer);
 
 // Gameboard - module, because we only need one
 const gameBoard = (() => {
@@ -42,7 +40,7 @@ const gameBoard = (() => {
     // b: ['4','5','6'],
     // c: ['7','8','9']}
 
-    let board = ['X','O','X','O','','X','X','O',''];
+    let board = new Array (9);
     let tiles = document.querySelectorAll(".gameboard__tile");
     
     // Function that will render the contents of the gameboard array to the webpage
@@ -62,20 +60,21 @@ const gameBoard = (() => {
 
 })()
 
-console.log(gameBoard.getTiles());
-console.log(gameBoard.getBoard());
 
-
-
-
-// Display Controller - module, because we only need one
+// Display Controller - modular pattern, because we only need one
 const displayController = (() => {
-    let tiles = document.querySelectorAll(".gameboard__tile");
 
-    tiles.forEach(tile => {
-        tile.addEventListener('click', function(e){
-            // e.target.innerHTML = 'X';
-            console.log(e.target)
+    // Enabling active user to place his mark on the board
+    gameBoard.getTiles().forEach(tile => {
+        tile.addEventListener('click',(e)=>{
+            let position = e.path[0].dataset.indexNumber-1;
+            let symbol = gameFlow.activePlayer.getSymbol()
+         
+            tile.textContent = symbol; // update the display
+            gameBoard.getBoard()[position] = symbol; // update the board array
+
+            console.log(gameBoard.getBoard());
+           
         })
     })
 
@@ -83,32 +82,3 @@ const displayController = (() => {
 
 
 
-// const Player = (name, level) => {
-//     let health = level * 2;
-//     const getLevel = () => level;
-//     const getName  = () => name;
-//     const die = () => {
-//       // uh oh
-//     };
-//     const damage = x => {
-//       health -= x;
-//       if (health <= 0) {
-//         die();
-//       }
-//     };
-//     const attack = enemy => {
-//       if (level < enemy.getLevel()) {
-//         damage(1);
-//         console.log(`${enemy.getName()} has damaged ${name}`);
-//       }
-//       if (level >= enemy.getLevel()) {
-//         enemy.damage(1);
-//         console.log(`${name} has damaged ${enemy.getName()}`);
-//       }
-//     };
-//     return {attack, damage, getLevel, getName};
-//   };
-
-//   const player1 = Player('Kaja','3');
-
-//   console.log(player1.getLevel())
