a8340de18de95885607ff030a603a062590233ec
Kaja Jaronska <kaja.jaronska@gmail.com>
2022-09-24 16:28:18 +0100

    Add event listener to the modal's submit button, which triggers function creating two players based on the user's input

--- script.js
+++ script.js
@@ -1,84 +1,102 @@
 // Factory function creating players
-const Player = (name,symbol) => {
-    let score = 0;
-    // Add variables to represent activePlayer, roundwinnner, gamewinner true/false 
-    const getName = () => name;
-    const getSymbol = () => symbol;
-    const getScore = () => score;
-    return {
-        getName,getScore,getSymbol   //shorthand
-    }
-}
+const Player = (name, symbol) => {
+  let score = 0;
+  // Add variables to represent activePlayer, roundwinnner, gamewinner true/false
+  const getName = () => name;
+  const getSymbol = () => symbol;
+  const getScore = () => score;
+  return {
+    getName,
+    getScore,
+    getSymbol, //shorthand
+  };
+};
 
-
-const player1 = Player('Kaja','X');
-const player2 = Player('Michael','O');
-
+const player1 = Player("Kaja", "X");
+const player2 = Player("Michael", "O");
 
 // Game flow module returning object - activePlayer
-const gameFlow = (()=> {
-    let activePlayer = player1;
+const gameFlow = (() => {
 
-    let roundWinner; // after every round 
-    let gameWinner;
-    let roundCounter; // decrease by one after every round, once ZERO - announce game winner
-    return {
-        activePlayer,
-        roundWinner,
-        gameWinner,
-        roundCounter
-    }
+    let player1,player2,roundCounter;
+
+  let gameSetup= () => {
+    let submitBtn = document.querySelector(".form__button");
+
+    submitBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      console.log("clickeeeeed!");
+
+        let playerOneName = document.getElementById('playerOneName').value;
+        let playerTwoName = document.getElementById('playerTwoName').value;
+        let roundCounter = document.getElementById('rounds').value;
+
+        player1 = Player(playerOneName, "X");
+        player2 = Player(playerTwoName, "O");
+
+        console.log(player1.getName(),player2.getName());
+
+        return player1,player2;
+
+    });
+  };
+
+  gameSetup();
+  console.log(player1,player2)
+  let activePlayer = player1;
+
+  let roundWinner; // after every round
+  let gameWinner;
+//   let roundCounter; // decrease by one after every round, once ZERO - announce game winner
+  return {
+    getPlayer1: () => player1,
+    getPlayer2: () => player2,
+    activePlayer,
+    roundWinner,
+    gameWinner,
+    roundCounter,
+  };
 })();
+
 
 
 // Gameboard - module, because we only need one
 const gameBoard = (() => {
+  // let board = {
+  //     // [0, 1, 2]
+  // a: ['1','2','3'],
+  // b: ['4','5','6'],
+  // c: ['7','8','9']}
 
-    // let board = {
-    //     // [0, 1, 2]
-    // a: ['1','2','3'],
-    // b: ['4','5','6'],
-    // c: ['7','8','9']}
+  let board = new Array(9);
+  let tiles = document.querySelectorAll(".gameboard__tile");
 
-    let board = new Array (9);
-    let tiles = document.querySelectorAll(".gameboard__tile");
-    
-    // Function that will render the contents of the gameboard array to the webpage
-    let displayBoard = () => {
-        
-        for (let i=0; i < tiles.length; i++) {        
-            tiles[i].textContent = board[i];
-        }
-               
+  // Function that will render the contents of the gameboard array to the webpage
+  let displayBoard = () => {
+    for (let i = 0; i < tiles.length; i++) {
+      tiles[i].textContent = board[i];
     }
-    displayBoard();
+  };
+  displayBoard();
 
-    return {
-        getTiles:() =>  tiles,
-        getBoard: () => board
-    };  
-
-})()
-
+  return {
+    getTiles: () => tiles,
+    getBoard: () => board,
+  };
+})();
 
 // Display Controller - modular pattern, because we only need one
 const displayController = (() => {
+  // Enabling active user to place his mark on the board
+  gameBoard.getTiles().forEach((tile) => {
+    tile.addEventListener("click", (e) => {
+      let position = e.path[0].dataset.indexNumber - 1;
+      let symbol = gameFlow.activePlayer.getSymbol();
 
-    // Enabling active user to place his mark on the board
-    gameBoard.getTiles().forEach(tile => {
-        tile.addEventListener('click',(e)=>{
-            let position = e.path[0].dataset.indexNumber-1;
-            let symbol = gameFlow.activePlayer.getSymbol()
-         
-            tile.textContent = symbol; // update the display
-            gameBoard.getBoard()[position] = symbol; // update the board array
+      tile.textContent = symbol; // updates the display
+      gameBoard.getBoard()[position] = symbol; // updates the board array
 
-            console.log(gameBoard.getBoard());
-           
-        })
-    })
-
-})()
-
-
-
+      console.log(gameBoard.getBoard());
+    });
+  });
+})();
